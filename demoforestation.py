865b0e7984dc05ca912eb41b7a6f566652ab3985
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-03-30 13:26:51 -0400

    main file

---
+++ demoforestation.py
@@ -0,0 +1,38 @@
+# This script will run every scipt in the demoforestation project
+
+# Load modules for making new directories
+
+import os
+
+# Declare path where the Demoforestation folder will be made
+
+filepath = 'C:/Users/User/Documents/Data/'
+
+# Make all drectories necessary for this project
+
+directories = ['Demoforestation', 'Demoforestation/Replication', 'Demoforestation/Specified']
+
+for d in directories:
+    
+    try:
+        
+        os.mkdir(filepath + d)
+    
+    except:
+        
+        continue
+
+# Load the scripts
+
+import demoforestation_replication as first # replication study
+import demoforestation_new_data as second # repeat above with new data
+import demoforestation_specified as third # panel data analysis
+import demoforestation_differenced as fourth # spatial econometrics analysis
+
+# Run them
+
+first
+second
+third
+fourth
+
