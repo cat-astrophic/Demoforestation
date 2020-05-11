86ee23d5b68a6a6d9adce6b624754b0b32ddf2c7
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-05-11 12:24:17 -0400

    panel data models

--- demoforestation_specified.py
+++ demoforestation_specified.py
@@ -2,6 +2,7 @@
 
 # Import required modules
 
+import numpy as np
 import pandas as pd
 import statsmodels.api as stats
 from ToTeX import restab
@@ -12,24 +13,29 @@ data = pd.read_csv('C:/Users/User/Documents/Data/demoforestation_panel.csv')
 
 # Data prep
 
-df1 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Continent']].dropna()
-df2 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', '2010']].dropna()
-df3 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Continent', '2010']].dropna()
-df4 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate', 'Continent']].dropna()
-df5 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate', '2010']].dropna()
-df6 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate', 'Continent', '2010']].dropna()
+lngdp = np.log(data['GDP_per_capita'])
+lngdp2 = np.log(data['GDP_per_capita_2'])
+data = pd.concat([data, lngdp, lngdp2], axis = 1)
+data.columns.values[-2:] = ['Ln_GDP', 'Ln_GDP_2']
+
+df1 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Continent']].dropna()
+df2 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', '2010']].dropna()
+df3 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Continent', '2010']].dropna()
+df4 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate', 'Continent']].dropna()
+df5 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate', '2010']].dropna()
+df6 = data[['Rate', 'Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate', 'Continent', '2010']].dropna()
 
 d1 = pd.get_dummies(df1['Continent'])
 d3 = pd.get_dummies(df3['Continent'])
 d4 = pd.get_dummies(df4['Continent'])
 d6 = pd.get_dummies(df6['Continent'])
 
-X1 = stats.add_constant(df1[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land']])
-X2 = stats.add_constant(df2[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', '2010']])
-X3 = stats.add_constant(df3[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', '2010']])
-X4 = stats.add_constant(df4[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate']])
-X5 = stats.add_constant(df5[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate', '2010']])
-X6 = stats.add_constant(df6[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ag_Land_Rate', '2010']])
+X1 = stats.add_constant(df1[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate']])
+X2 = stats.add_constant(df2[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', '2010']])
+X3 = stats.add_constant(df3[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', '2010']])
+X4 = stats.add_constant(df4[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate']])
+X5 = stats.add_constant(df5[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate', '2010']])
+X6 = stats.add_constant(df6[['Democracy', 'Democracy_2', 'Education', 'Rural_Pop', 'Ln_Land', 'Ln_GDP', 'Ln_GDP_2', 'Tariff_Rate', 'Ag_Land_Rate', '2010']])
 
 X1 = X1.join(d1).drop('Oceania', axis = 1)
 X3 = X3.join(d3).drop('Oceania', axis = 1)
