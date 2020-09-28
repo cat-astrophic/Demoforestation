a45d8f495950adf4d9e000273a9a4cb3a5fcb56e
Michael Cary <39915578+cat-astrophic@users.noreply.github.com>
2020-09-28 11:56:39 -0400

    robustness checks for differenced models

---
+++ demoforestation_differenced_ROB2.py
@@ -0,0 +1,55 @@
+# This script runs expanded econometric models using both old and new data - ROBUST 2
+
+# Import required modules
+
+import pandas as pd
+import statsmodels.api as stats
+from ToTeX import restab
+
+# Reading in the data
+
+data = pd.read_csv('C:/Users/User/Documents/Data/demoforestation_differenced.csv')
+
+# Add Urbanization squared to the data for the robustness checks
+
+U = pd.Series(data.Urbanization*data.Urbanization, name = 'Urbanization_2')
+data = pd.concat([data, U], axis = 1)
+
+# Data prep
+
+dXi = data['Urbanization']*data['GDP_per_capita']
+data = pd.concat([data, pd.DataFrame(dXi, columns = ['U X GDP'])], axis = 1)
+
+df1 = data[['Rate', 'Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP', 'Continent']].dropna()
+df2 = data[['Rate', 'Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP', 'Ag_Land_Rate', 'Continent']].dropna()
+df3 = data[['Rate', 'Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP', 'Ag_Land_Rate', 'Tariff_Rate', 'Continent']].dropna()
+
+d1 = pd.get_dummies(df1['Continent'])
+d2 = pd.get_dummies(df2['Continent'])
+d3 = pd.get_dummies(df3['Continent'])
+
+X1 = stats.add_constant(df1[['Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP']])
+X2 = stats.add_constant(df2[['Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP', 'Ag_Land_Rate']])
+X3 = stats.add_constant(df3[['Urbanization', 'Urbanization_2', 'Education', 'Rural_Pop', 'Ln_Land', 'GDP_per_capita', 'U X GDP', 'Ag_Land_Rate', 'Tariff_Rate']])
+
+X4 = X1.join(d1).drop('Oceania', axis = 1)
+X5 = X2.join(d2).drop('Oceania', axis = 1)
+X6 = X3.join(d3).drop('Oceania', axis = 1)
+
+# Running regressions and saving results
+
+Ys = [df1['Rate'], df2['Rate'], df3['Rate'], df1['Rate'], df2['Rate'], df3['Rate']]
+Xs = [X1, X2, X3, X4, X5, X6]
+res_list = []
+
+for i in range(len(Xs)):
+
+    res = stats.OLS(Ys[i],Xs[i]).fit(cov_type = 'HC1')
+    res_list.append(res)
+    print(res.summary())
+    file = open('C:/Users/User/Documents/Data/Demoforestation/Specified/Differenced_Model_ROB2_' + str(i+1) + '.txt', 'w')
+    file.write(res.summary().as_text())
+    file.close()
+
+restab(res_list, 'C:/Users/User/Documents/Data/Demoforestation/Specified/restab_differenced_ROB2.txt')
+
